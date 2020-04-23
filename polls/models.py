1a58d7e1d731c3f7c11cd2a18b28091cf60e990f
yuu-kannagi <Yuu.kannagi23@gmail.com>
2020-04-23 12:21:18 +0900

    modelの作成

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class question(models.model):
+    question_text = models.ChaField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class choice(models.model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
