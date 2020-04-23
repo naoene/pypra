664d59c9c3a3a6e1d039a472add00b4cacd9a8c2
yuu-kannagi <Yuu.kannagi23@gmail.com>
2020-04-23 14:32:33 +0900

    誤字の修正

--- gozaru/models.py
+++ gozaru/models.py
@@ -4,12 +4,12 @@ from django.db import models
 from django.db import models
 
 
-class question(models.model):
+class question(models.Model):
     question_text = models.ChaField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
 
-class choice(models.model):
+class choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
