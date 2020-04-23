7e7f70ce499365785fbda2c19cd7455dea54ee64
yuu-kannagi <Yuu.kannagi23@gmail.com>
2020-04-23 14:39:53 +0900

    変更

--- gozaru/models.py
+++ gozaru/models.py
@@ -5,11 +5,11 @@ from django.db import models
 
 
 class question(models.Model):
-    question_text = models.ChaField(max_length=200)
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
 
 class choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
