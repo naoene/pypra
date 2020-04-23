f17653b3026e8d9e22a7198052a34c75c3fa931c
yuu-kannagi <Yuu.kannagi23@gmail.com>
2020-04-23 19:23:21 +0900

    モデルの修正

--- gozaru/admin.py
+++ gozaru/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-# from .models import post
+from .models import Question, Choice
 
-# admin.site.register(post)
+admin.site.register(Question)
+admin.site.register(Choice)
