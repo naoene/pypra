55a52aa9044080959f50c51db4721808bf080f01
yuu-kannagi <Yuu.kannagi23@gmail.com>
2020-05-03 15:57:40 +0900

    編集

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ('-date_posted')
